60afb7ec5ed47f87c85e6cb8686e74d3e52285f1
Guilherme <guilherme.sylva92@gmail.com>
2023-05-01 11:54:17 -0300

    Ajuste para correção de build

--- src/layout/Home/Home.tsx
+++ src/layout/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Banner from "../../components/Banner";
 import Sobre from "../../components/Sobre";
-import WhoAmI from "../../components/WhoAmI";
+
 
 import * as Styles from "./Home.styles";
 
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../layout/Home";
-import Sobre from "../components/Sobre";
+
 
 const Router = () => {
   return (
